515e8d36397af31ddf7b2eb9ac979e3d92c6b543
RonjasJ <30828377+RonjasJ@users.noreply.github.com>
2018-02-22 16:39:59 +0530

    Create holiday.c

---
+++ holiday.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+void main()
+{
+    char str[20];
+    printf("Enter the day");
+    scanf("%s",str);
+    if((str[0]=='s'))
+    printf("Yes");
+    else
+    printf("No");    
+}
