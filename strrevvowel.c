2faccaae72d6439e380a68e14473529cafeede4f
RonjasJ <30828377+RonjasJ@users.noreply.github.com>
2018-02-27 15:59:50 +0530

    Create strrevvowel.c

---
+++ strrevvowel.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+#include<string.h>
+int main()
+{
+	char s[30],p[30],r[30];
+	int i,l,j=0,k=0;
+	printf("Enter the String:");
+	scanf("%s",s);
+	l=strlen(s);
+	for(i=0;i<l;i++)
+	{
+		if((s[i]=='a')||(s[i]=='e')||(s[i]=='i')||(s[i]=='o')||(s[i]=='u')||(s[i]=='A')||(s[i]=='E')||(s[i]=='I')||(s[i]=='O')||(s[i]=='U'))
+		{
+			p[j]=s[i];yu
+			j++;
+		}
+		else
+		{
+			r[k]=s[i];
+			k++;
+		}
+	}
+	strrev(r);
+	printf("\n%s",r);
+}
