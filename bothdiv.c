b985e59ec2695004a128fd5d279ba47373926840
RonjasJ <30828377+RonjasJ@users.noreply.github.com>
2018-02-27 16:16:59 +0530

    Create bothdiv.c

---
+++ bothdiv.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+int l,r,i;
+printf("enter the two numbers");
+scanf("%d %d",&l,&r);
+for(i=r;i<10000;i++)
+{
+	if(i%l==0&&i%r==0)
+	{
+		break;
+}
+}
+	printf("%d",i);
+ return 0;
+}
