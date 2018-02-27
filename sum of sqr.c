7b742e4c348d9366f0a42f513e29c4603f507209
RonjasJ <30828377+RonjasJ@users.noreply.github.com>
2018-02-27 15:53:31 +0530

    Create sum of sqr.c

---
+++ sum of sqr.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+	int n,r,sum=0;
+	printf("Enter the Number:");
+	scanf("%d",&n);
+	while(n!=0)
+	{
+		r=n%10;
+		r=r*r;
+		sum=sum+r;
+		n=n/10;
+	}
+	printf("\nThe Sum of Square of Digits is %d",sum);
+   return 0;
+}
