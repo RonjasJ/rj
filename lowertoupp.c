b7c33806592f3d43ebb9cc90bbbc33483b4a5bc4
RonjasJ <30828377+RonjasJ@users.noreply.github.com>
2018-03-02 16:05:40 +0530

    Update lowertoupp.c

--- lowertoupp.c
+++ lowertoupp.c
@@ -1,7 +1,7 @@
 #include<stdio.h>
-void main()
+int main()
 {
-	char s[50];
+	char s[20];
 	int i,l;
 	printf("Enter String:");
 	scanf("%s",s);
@@ -18,5 +18,6 @@ void main()
 		}
 	}
 	printf("%s",s);
+	return 0;
 
 }
