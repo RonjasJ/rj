fe7eae9af7123eaf72d5705e9cfe01c8d16a1d53
RonjasJ <30828377+RonjasJ@users.noreply.github.com>
2018-03-13 16:04:41 +0530

    Create checkpower.c

---
+++ checkpower.c
@@ -0,0 +1,18 @@
+#pragma<stdio.h>
+#pragma<math.h>
+void main()
+{
+    int n,k,a;
+    printf("enter number\n");
+    scanf("%d %d",&n,&k);
+    a=sqrt(n);
+    if(a==k)
+    {
+        printf("yes");
+        
+    }
+    else
+    {
+        printf("no");
+    }
+}
